8538aad3815d723e5c433f2cb4d0ebb0b8917ee6
Manuel Covas <manuel.p.covas@outlook.com>
2020-01-12 05:58:00 +0000

    Added handling for disconnects.

--- src/tcpserial.py
+++ src/tcpserial.py
@@ -2,7 +2,7 @@ import serial
 import socket
 
 TCP_ADDRESS = "0.0.0.0"
-TCP_BUFFER_SIZE = 2**6
+TCP_BUFFER_SIZE = 2**14
 
 
 serial_port = serial.Serial()
@@ -19,24 +19,30 @@ tcp_socket.bind((TCP_ADDRESS, tcp_port))
 tcp_socket.listen()
 
 
+def address_string(tuple_address):
+    return tuple_address[0]+":"+str(tuple_address[1])
+
 while True:
     print("Waiting for connection...")
     connection, addr = tcp_socket.accept()
-    connection.settimeout(0.010)
-    print("Connection from "+"".join(tuple(map(str, addr))))
+    connection.settimeout(0.001)
+    print("Connection from "+address_string(addr))
 
     while True:
         try:
             data = connection.recv(TCP_BUFFER_SIZE)
+            if len(data) == 0:
+                break
             written = serial_port.write(data)
-            print(">> "+str(written)+" bytes")
+            print(">> "+str(written)+"/"+str(len(data))+" bytes")
         except:
             pass
 
         if serial_port.in_waiting != 0:
+            total = serial_port.in_waiting
             data = serial_port.read(serial_port.in_waiting)
             written = connection.send(data)
-            print("<< "+str(written)+" bytes")
+            print("<< "+str(written)+"/"+str(total)+" bytes")
 
     connection.close()
-    print("Closed connection to "+"".join(tuple(map(str, addr))))
+    print("Closed connection to "+address_string(addr))
